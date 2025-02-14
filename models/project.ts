047e7161deb2b162e22faeb4ce9e5abe1355d0e8
Anurag Sidhu <anuragsidhu72@gmail.com>
2025-02-14 13:22:00 +0530

    k

--- models/project.ts
+++ models/project.ts
@@ -1,28 +1,44 @@
 import mongoose, { Document, Schema } from 'mongoose';
+
 // Interface for the Project document
 interface IProject extends Document {
     projectId: string;
     userId: string;
     folders: string[];
-    files: { name: string; content: string; folder?: string }[];
-    projectName: string
-    email: string
+    files: {
+        name: string;
+        content: string;
+        folder?: string;
+        imports: string[];
+        exports: string[];
+        externalLibraries: string[];
+    }[];
+    projectName: string;
+    email: string;
 }
+
 // Define the schema for the Project model
-const projectSchema = new Schema<IProject>({
-    projectId: { type: String, sparse: true, unique: true },
-    userId: { type: String },
-    folders: { type: [String], default: [] },
-    projectName: { type: String },
-    email: { type: String },
-    files: [
-        {
-            name: { type: String, required: true },
-            content: { type: String, required: true },
-            folder: { type: String, default: '' },
-        },
-    ],
-}, { timestamps: true });
+const projectSchema = new Schema<IProject>(
+    {
+        projectId: { type: String, sparse: true, unique: true },
+        userId: { type: String },
+        folders: { type: [String], default: [] },
+        projectName: { type: String },
+        email: { type: String },
+        files: [
+            {
+                name: { type: String, required: true },
+                content: { type: String, required: true },
+                folder: { type: String, default: '' },
+                imports: { type: [String], default: [] },
+                exports: { type: [String], default: [] },
+                externalLibraries: { type: [String], default: [] },
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
 // Create the Project model
 const Project = mongoose.models.Project || mongoose.model<IProject>('ProjectData', projectSchema);
 export default Project;
